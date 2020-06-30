af04a5495351582d632845c39a81e14fdf948ea1
Sam van der Kris <samvdkris@disroot.org>
2020-06-30 23:46:16 +0200

    Add koa-router

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 "use strict";
 import * as Koa from "koa";
+import * as KoaRouter from "koa-router";
+
 const app = new Koa();
+const router = new KoaRouter();
 
 
-app.use(async ctx => {
-	ctx.body = "Hello world!";
+router.get("/", ctx => {
+	ctx.body = "Hello index";
 });
 
+router.get("/test", ctx => {
+	ctx.body = "Hello test";
+});
+
+
+// Router middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log("Server started..."));
